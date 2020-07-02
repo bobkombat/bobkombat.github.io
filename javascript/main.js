a08498b71ce5db89e701efea7a06e9e889787a3b
bobkombat <rdhwnrmdhny@gmail.com>
2020-07-02 13:54:24 +0700

    1

--- javascript/main.js
+++ javascript/main.js
@@ -1,52 +1,88 @@
-console.log("hello World");
-console.log(`____________`)
-console.log('woidbiowd')
-document.body.id = "#wakwaw";
-console.log(document.body.id);
+import {setCons, aStar} from './a*.js';
+// console.log(x);
 
-console.log(document.scripts[0]);
+let test = document.getElementById("test2");
 
-var x = document.getElementById("#wakwaw")
-console.log(x);
-var para = document.createElement("P");
-para.innerText = "lmao";
-document.body.appendChild(para);
-
-var list = [...Array(5).keys()];
-
-for (let i = 0; i < list.length; i++)
-{
-  var para = document.createElement("P");
-  para.innerText = list[i];
-  document.body.appendChild(para);
-}
-
-var z = document.querySelector('body');
-console.log(z);
-
-
-var div = document.getElementsByTagName('p')
-console.log(div);
-
-console.log(div.length);
-console.log(typeof div);
-div[1].style.background = 'blue';
-//console.log(div[0].textContent);
-
-for (let i = 0; i < 10; i++)
+//CREATE THE DIV GRID;
+// function setGrid(x, y) {};
+for (let i = 0; i < 30; i++)
 {
   let div1 = document.createElement("div");
+  div1.id = "test3";
   div1.className = "test3";
-  let x = document.getElementsByClassName('test2');
-  document.body.appendChild(div1);
-  for (let j = 0; j < 20; j++)
+  document.getElementById("test2").appendChild(div1);
+  for (let j = 0; j < 90; j++)
   {
     let div2 = document.createElement("div");
     div2.className = "test4";
-    let z = document.getElementsByClassName('test3');
-    document.body.appendChild(div2);
+    let set = document.querySelectorAll("#test3");
+    document.getElementsByClassName("test3")[i].appendChild(div2);
   }
 }
 
-var flex = document.getElementsByClassName('test4')
-console.log(flex);
+// VAR FOR IMPORT
+//array of row divs
+const arr1 = document.querySelectorAll(".test3");
+const listArr1 = new Array(...arr1);
+
+//array of column divs
+const arr2 = arr1[0].childNodes;
+
+//length of each array
+const length1 = arr1.length;
+const length2 = arr2.length;
+
+//SET THE BACKGROUND COLOR
+for (let i = 0; i < length1; i++)
+{
+  for (let j = 0; j < length2; j++)
+  {
+    //if (j % 2)
+      //arr1[i].childNodes[j].style.background = "#d0d0d0";
+  }
+}
+
+
+//ARRAY
+var array = new Array(length1);
+for (let i = 0; i < length1; i++)
+{
+  array[i] = new Array(length2);
+}
+// console.log(array[0]);
+
+//start and end color;
+arr2[0].style.background = "lime";
+arr1[length1 - 1].childNodes[length2 - 1].style.background = "red";
+
+//START, END VALUE
+  //start
+let start;
+for (let i = 0; i < length1; i++)
+{
+  for (let j = 0; j < length2; j++) {
+    const x = arr1[i].childNodes[j].style.background.split(' ');
+    if (x[0] == "lime") {
+      start = [i, j];
+      break;
+    }
+  }
+}
+  //end
+let end;
+for (let i = length1 - 1; i >= 0; i--)
+{
+  for (let j = length2 - 1; j >= 0; j--) {
+    const x = arr1[i].childNodes[j].style.background.split(' ');
+    if (x[0] == "red") {
+      end = [i, j];
+      break;
+    }
+  }
+}
+
+//Set value
+setCons(array);
+console.log(array[0][0]);
+
+export {arr1, listArr1, arr2, length2, length1, array, start, end};
